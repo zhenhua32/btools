6d30fcaae1dfc47b3803425ebd7fa8d009b44a81
zhenhua32 <zhenhua32@gmail.com>
2026-03-08 21:47:33 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,16 @@ import router from './router'
 import 'virtual:uno.css'
 import './styles/global.css'
 
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+
+self.MonacoEnvironment = {
+  getWorker(_: unknown, label: string) {
+    if (label === 'json') return new jsonWorker()
+    return new editorWorker()
+  },
+}
+
 const app = createApp(App)
 app.use(router)
 app.use(naive)
